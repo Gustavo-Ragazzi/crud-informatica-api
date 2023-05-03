b170ec82eba43cd302caf384efe1889cc08a9fce
Gustavo-Ragazzi <ragazzi@hotmail.com.br>
2023-05-03 18:42:59 -0300

    remove cors authentication

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Importe o módulo 'cors'
 const app = express();
 const routes = require('./routes/routes');
 
 app.use(express.json());
+app.use(cors()); // Adicione o middleware 'cors'
 app.use('/api', routes);
 
 app.listen(8000, () => {
